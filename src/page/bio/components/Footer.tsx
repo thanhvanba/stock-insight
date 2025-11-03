fd15f74215569e5e43c721b32e7f867beb2df366
Nguyen Kim An <annk.sale@gmail.com>
2025-11-03 11:44:53 +0700

    refactor: bio Footer add interface

--- src/page/bio/components/Footer.tsx
+++ src/page/bio/components/Footer.tsx
@@ -1,6 +1,26 @@
-export default function Footer({ isSticky }) {
-  const currentYear = new Date().getFullYear();
+interface FooterProps {
+  isSticky: boolean;
+}
+// export default function Footer({ isSticky }) {
+//   const currentYear = new Date().getFullYear();
 
+//   return (
+//     <div className="max-w-sm mx-auto flex justify-center">
+//       <p
+//         className={`absolute ${
+//           isSticky ? "sticky mt-4 bottom-4" : "static bottom-4"
+//         }  text-xs`}
+//       >
+//         Copyright ©️ {currentYear} Created by{" "}
+//         <span className="font-bold">
+//           <a href="https://bio.agengari.my.id/">RedCode Team & Mr. Tuấn</a>
+//         </span>
+//       </p>
+//     </div>
+//   );
+// }
+const Footer: React.FC<FooterProps> = ({ isSticky }) => {
+  const currentYear = new Date().getFullYear();
   return (
     <div className="max-w-sm mx-auto flex justify-center">
       <p
@@ -15,4 +35,6 @@ export default function Footer({ isSticky }) {
       </p>
     </div>
   );
-}
+};
+
+export default Footer;
